178969a5f41fa130a7d9475fec7aba831268879a
Aarthini-Manickam <82097012+Aarthini-Manickam@users.noreply.github.com>
2021-04-12 17:49:26 +0530

    cart page added

---
+++ components/BottomRepBlock.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="common-rep-block smartphone-only">
+    <SfHeading
+      class="heading-offer"
+      :title="$t('Get Exclusive Special Offers & The Latest News')"
+    />
+    <SfLink class="sf-product-card__link subscribePDP"
+      >SUBSCRIBE / SIGN UP >
+    </SfLink>
+    <div class="smartphone-only">
+      <SfButton
+        class="sf-add-to-cart__button atbbtnPDP repButton"
+        :disabled="loading"
+        @click="
+          addItem({
+            product,
+            quantity: parseInt(qty),
+            repId: 'rep01',
+          })
+        "
+      >
+        Find a Representative
+      </SfButton>
+      <SfButton
+        class="sf-add-to-cart__button atbbtnPDP repButton"
+        :disabled="loading"
+        @click="
+          addItem({
+            product,
+            quantity: parseInt(qty),
+            repId: 'rep01',
+          })
+        "
+      >
+        Become a Representative
+      </SfButton>
+    </div>
+  </div>
+</template>
+<script>
+import { SfButton, SfHeading, SfLink } from "@storefront-ui/vue";
+export default {
+  name: "BottomRepBlock",
+  components: {
+    SfHeading,
+    SfButton,
+    SfLink,
+  },
+  setup(props, context) {},
+};
+</script>
+<style>
+.subscribePDP {
+  color: var(--c-primary);
+  text-align: center;
+  align-items: center;
+  line-height: 2rem;
+  font-family: var(--font-family);
+}
+.atbbtnPDP {
+  font-size: 10pt;
+  font-weight: bold;
+  background-color: var(--c-primary);
+  color: white;
+  height: 40px !important;
+  border-radius: 5px;
+  margin: 0px;
+  font-family: var(--font-family);
+}
+.repButton {
+  margin: 10px auto;
+  width: 380px;
+}
+</style>
---
+++ components/CartProduct.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+    <div class="cart_products">
+      <div class="cart_product_image">
+        <nuxt-link data-cy="cart_product_link" :to="link">
+          <SfImage
+            :src="image"
+            alt="cart_product_image"
+            :width="75"
+            :height="75"
+          />
+        </nuxt-link>
+      </div>
+      <div class="cart_product_title">
+        <nuxt-link data-cy="cart_product_link" :to="link">
+          <SfHeading
+            :level="4"
+            :title="title"
+            class="filters__title sf-heading--left"
+          />
+        </nuxt-link>
+      </div>
+      <div class="cart_itemprice">
+        <SfPrice
+          class="sf-product-card__price productPrice"
+          :regular="regularprice"
+        />
+        <SfHeading :level="5" :title="$t('Inc. VAT')" class="cart_vat" />
+        <SfHeading
+          :level="6"
+          :title="$t('Excludes delivery')"
+          class="cart_delivery_price"
+        />
+      </div>
+      <div class="cart_quantity">
+        <div class="qnty">
+          <p>{{ qty }}</p>
+        </div>
+        <div class="cart_remove" @click="$emit('click:remove')">
+          <!-- <SfIcon
+            icon="cross"
+            size="xxs"
+            color="#7f28c4"
+            viewBox="0 0 24 24"
+            :coverage="1"
+            class="remove_icon"
+          /> -->
+          x remove
+        </div>
+      </div>
+      <div class="cart_subtotalprice desktop-only">
+        <SfPrice
+          class="sf-product-card__price productPrice"
+          :regular="regularprice"
+        />
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+import {
+  SfHeading,
+  SfImage,
+  SfLink,
+  SfPrice,
+  SfInput,
+  SfIcon,
+  SfTable,
+} from "@storefront-ui/vue";
+export default {
+  name: "CartProduct",
+  components: {
+    SfHeading,
+    SfImage,
+    SfLink,
+    SfPrice,
+    SfInput,
+    SfTable,
+    SfIcon,
+  },
+  props: ["title", "image", "regularprice", "specialprice", "qty", "link"],
+  setup(props, context) {
+    console.log("props1234", props, context);
+    const onRemove = (e) => {
+      event.preventDefault();
+      console.log("test...............", context);
+      context.root.$emit("click:shadeChange", title);
+    };
+    const activeShadeClasses = (title) => {
+      console.log("title>>>>>>>>>", title, props.selectedShade);
+      let defaultClass = "shadeDet";
+      if (title === props.selectedShade) {
+        return `${defaultClass} ${"shadeActive"}`;
+      } else {
+        return `${defaultClass} ${"shadeInactive"}`;
+      }
+    };
+    return {
+      onRemove,
+      activeShadeClasses,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.cart_products {
+  @include for-desktop {
+    display: flex;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 160px;
+  }
+  @include for-mobile {
+    display: grid;
+    margin: 0px 20px;
+  }
+  .cart_product_image {
+    @include for-mobile {
+      grid-column: 1;
+      grid-row: 1;
+    }
+  }
+  .cart_product_title {
+    @include for-mobile {
+      grid-column: 2 / span 2;
+      grid-row: 1;
+    }
+    @include for-desktop {
+      width: 290px;
+    }
+  }
+  .cart_itemprice {
+    @include for-mobile {
+      grid-column: 1;
+      grid-row: 2;
+      margin-top: 30px;
+    }
+    @include for-desktop {
+      margin-top: 10px;
+    }
+    .cart_vat {
+      text-align: left;
+      margin-bottom: -15px;
+    }
+    .cart_delivery_price {
+      @include for-desktop {
+        margin-top: 10px;
+      }
+    }
+  }
+  .cart_quantity {
+    @include for-mobile {
+      grid-column: 2;
+      grid-row: 2;
+    }
+    .qnty {
+      border: 1px solid black;
+      width: 75px;
+      height: 50px;
+      border-radius: 5px;
+      margin-top: 20px;
+      text-align: center;
+    }
+    .remove_icon {
+      float: left;
+      margin-top: 5px;
+      margin-right: 5px;
+    }
+    .cart_remove {
+      font-family: var(--font-family);
+      font-weight: bolder;
+      font-size: 15px;
+      cursor: pointer;
+    }
+  }
+  .cart_subtotalprice {
+    @include for-mobile {
+      grid-column: 3;
+      grid-row: 2;
+      margin-top: 30px;
+    }
+  }
+}
+</style>
