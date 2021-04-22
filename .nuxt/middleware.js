6e0c0d642819028c97d18cc5bfbf286087ce8f41
AdityaKRaghuvanshi <aditya.raghuvanshi@hcl.com>
2021-04-22 11:21:42 +0530

    Untrack files in .gitignore

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['checkout'] = require('../middleware/checkout.js')
-middleware['checkout'] = middleware['checkout'].default || middleware['checkout']
-
-middleware['is-authenticated'] = require('../middleware/is-authenticated.js')
-middleware['is-authenticated'] = middleware['is-authenticated'].default || middleware['is-authenticated']
-
-export default middleware
--- .nuxt/plugins.logger.ffb89e78.js
+++
@@ -1,8 +0,0 @@
-import { registerLogger } from '@vue-storefront/core'
-
-const loggerPlugin = (app) => {
-  const { verbosity, customLogger, ...args } = {};
-  registerLogger(customLogger || args, verbosity || 'error')
-};
-
-export default loggerPlugin;
